cdbb2259e674fbd3a1b1d80cec91a0e9f81085da
Adrimrtz16 <4281885@alu.murciaeduca.es>
2025-06-02 19:34:29 +0200

    esquemas

--- src/main/java/TeamDex/TeamDex/TeamDexApplication.java
+++ src/main/java/TeamDex/TeamDex/TeamDexApplication.java
@@ -2,8 +2,10 @@ package TeamDex.TeamDex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class TeamDexApplication {
 
 	public static void main(String[] args) {
